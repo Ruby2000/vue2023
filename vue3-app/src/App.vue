f66d7017a89b80469c0ca8ba80ffee0348cb3739
Ruby2000 <87626351@qq.com>
2023-07-18 11:16:46 +0800

    组合式api

--- vue3-app/src/App.vue
+++ vue3-app/src/App.vue
@@ -2,28 +2,19 @@
  <button @click="increment">count is :{{ count }}</button>
 </template>
 
-<script>
-export default {
-//data() 返回的属性将会成为响应式的状态
-//  并且暴露在'this'上
-  data(){
-    return{
-      count: 0
-    }
-  },
-//  methods是一些用来更改状态与触发更新的函数
-//  它们可以在模板中作为事件处理器绑定
-  methods:{
-    increment() {
-      this.count++
-    }
-  },
-//  生命周期钩子会在组件生命周期的各个不同阶段被调用
-//  例如这个函数就会在组件挂载完成后被调用
-  mounted() {
-    console.log('The initial count is ${this.count}.')
-  }
+<script setup>
+import { ref, onMounted } from 'vue'
+//响应式状态
+const  count = ref(0)
+//用来修改状态、触发更新的函数
+function increment(){
+  count.value++
 }
+//生命周期钩子
+onMounted(() => {
+  console.log('The initial count is ${count.value}.')
+    }
+)
 </script>
 
 <style>
