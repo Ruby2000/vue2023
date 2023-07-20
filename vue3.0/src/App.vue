2254f20ec123d2816e66d24680a4558831dd82b6
Ruby2000 <87626351@qq.com>
2023-07-20 14:39:24 +0800

    shallowRef响应式和非响应式

--- vue3.0/src/App.vue
+++ vue3.0/src/App.vue
@@ -1,56 +1,31 @@
 <template>
   <div>
-    <h1>{{num.y}}</h1>
-    <h1>{{num1.y}}</h1>
-    <button @click="num.y++">num ++ 1</button>
-    <button @click="num1.y++">num1 ++ 1</button>
-    <hr>
-    <h1>{{person}}</h1>
-    <h3>姓名：{{ name }}</h3>
-    <h3>年龄：{{ age }}</h3>
-    <h3>薪资：{{ content.fit.salary }}</h3>
-    <hr>
-    <button @click="name+=`--`">修改名字</button>
-    <button @click="age++">修改年龄</button>
-    <button @click="content.fit.salary++">修改年薪</button>
+
+<!--    <h1>姓名 : {{sum.n}}</h1>-->
+<!--    <button @click="sum.n++">点击+</button>-->
+
+        <h1>姓名 : {{sum}}</h1>
+        <button @click="sum++">点击+</button>
   </div>
 </template>
 
 <script>
-import { shallowRef, shallowReactive,toRefs } from 'vue';
+import { shallowRef} from 'vue';
 export default {
   name: "App",
   setup() {
-    // shallowReactive 浅响应式
-    let person = shallowReactive({
-      name:"张三",
-      age:18,
-      content:{
-        fit:{
-          salary:20
-        }
-      }
-    })
-    // 被 shallowRef 包裹的对象无法实现响应式
-    let num = shallowRef({
-      y:0
-    })
-    let num1 = shallowRef({
-      y:0
-    })
+     let sum =shallowRef(
+         0// {n: 0}
+     )
+
     return {
-      num,
-      num1,
-      ...toRefs(person)
+      sum,
+
     };
   }
 };
 </script>
 
 <style>
-#app {
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
